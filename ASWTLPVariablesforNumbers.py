8517a4d602abe4132d208909ae88ddf8ee6c12f8
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-13 00:02:39 -0400

    final commit for Python Variable for Numbers

--- ASWTLPVariablesforNumbers.py
+++ ASWTLPVariablesforNumbers.py
@@ -1,19 +1,46 @@
 # There are two correct answers for this one. Answer with either one. What is this?
 # 100
 import numbers
+import string
 numbers
+
 # Type the illegal character in this variable name.
 # 2nd_runner_up
 2
+
 # Assign the number 5 to the variable players
 players = 5
 # Assign the negative number -6 to the variable loss
 loss = -6
+
 # What is the value of total?
 # number = 10
 # total = number + 2
 12
+
 # Assign a number (your choice) to the variable total
 total = 10
+
 # Assign a negative integer to a variable. You choose the integer and the variable name.
 cost = -2
+
+# What is this (one word, lowercase)?
+# "-8.087"
+string
+
+# Add two variables and assign the result to a third variable. Make up all the variable names.
+total = 1
+cost =10
+order_total = total + cost
+
+# Add 5 to the variable running_total and assign the result to the same variable
+running_total = 5
+running_total = running_total + 5
+
+# Assign the sum of two numbers to a variable. Make up the variable name and the numbers, tell Python to display the sum using the variable.
+cost = 5 + 5
+
+# Assigned a number to a variable, divide the variable by a number of your choice. Assign the result to a variable of your choice ,tell Python to display the result using that second variable.
+total = 102.20
+total = total /2
+print(total)
