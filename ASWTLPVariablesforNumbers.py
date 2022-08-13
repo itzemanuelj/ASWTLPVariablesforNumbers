84a234facd9bbb4495a7e31f234b983c827f5c2d
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-12 21:25:33 -0400

    inital halway commit

---
+++ ASWTLPVariablesforNumbers.py
@@ -0,0 +1,19 @@
+# There are two correct answers for this one. Answer with either one. What is this?
+# 100
+import numbers
+numbers
+# Type the illegal character in this variable name.
+# 2nd_runner_up
+2
+# Assign the number 5 to the variable players
+players = 5
+# Assign the negative number -6 to the variable loss
+loss = -6
+# What is the value of total?
+# number = 10
+# total = number + 2
+12
+# Assign a number (your choice) to the variable total
+total = 10
+# Assign a negative integer to a variable. You choose the integer and the variable name.
+cost = -2
